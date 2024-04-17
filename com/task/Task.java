8d47eb6d6b9049726867bcbdf2e092a139d90e32
Lilian <alexandru-lilian.natarau@asml.com>
2024-04-17 14:44:25 +0300

    set enum via ordinal

--- com/task/Task.java
+++ com/task/Task.java
@@ -3,12 +3,13 @@ package com.task;
 public class Task {
 	private int id;
 	private String desc;
+	private TaskType taskType;
 
 	public void setId(int id) { this.id = id; }
 	public void setDesc(String desc) { this.desc = desc; }
 	public String getDesc() {return this.desc; }
 
 	public String toString() {
-		return "{ id: " + id + ", " + "desc: " + desc + "}";
+		return "{ id: " + id + ", " + "desc: " + desc + ", taskType: " + taskType.name() + "}";
 	}
 }
--- com_test_Test.c
+++ com_test_Test.c
@@ -45,24 +45,45 @@ JNIEXPORT jobject JNICALL Java_com_test_Test_dummyTasks(JNIEnv *env, jobject obj
 	// Get the constructor method id
 	jmethodID constructor = (*env)->GetMethodID(env, task_class, "<init>", "()V");
 
+	// get enum related objects
+	// Find the class of MyEnum
+	jclass myEnumClass = (*env)->FindClass(env, "com/task/TaskType");
+	
+	// Get the values() method of MyEnum
+	jmethodID valuesMethod = (*env)->GetStaticMethodID(env, myEnumClass, "values", "()[Lcom/task/TaskType;");
+	
+	// Call the values() method to get an array of enum constants
+	jobjectArray enumConstants = (*env)->CallStaticObjectMethod(env, myEnumClass, valuesMethod);
+	
+	
+	
 	// Get the field ids
 	jfieldID idField = (*env)->GetFieldID(env, task_class, "id", "I");
+	jfieldID typeField = (*env)->GetFieldID(env, task_class, "taskType", "Lcom/task/TaskType;");
 	jfieldID msgField = (*env)->GetFieldID(env, task_class, "desc", "Ljava/lang/String;");
-
+	
 	// Create a new Task object
 	jobject myObj = (*env)->NewObject(env, task_class, constructor);
-
+	
 	// Set the field values
 	(*env)->SetIntField(env, myObj, idField, 1);
 	(*env)->SetObjectField(env, myObj, msgField, (*env)->NewStringUTF(env, "hello"));
-
+	
+	// Retrieve the desired enum constant from the array using its ordinal
+	jobject enumConstant = (*env)->GetObjectArrayElement(env, enumConstants, 0);
+	(*env)->SetObjectField(env, myObj, typeField, enumConstant);
+	
 	// Create a second Task object
 	jobject mySecondObj = (*env)->NewObject(env, task_class, constructor);
-
+	
 	// Set the field values
 	(*env)->SetIntField(env, mySecondObj, idField, 2);
 	(*env)->SetObjectField(env, mySecondObj, msgField, (*env)->NewStringUTF(env, "bye"));
-
+	
+	// Retrieve the desired enum constant from the array using its ordinal
+	enumConstant = (*env)->GetObjectArrayElement(env, enumConstants, 1);
+	(*env)->SetObjectField(env, mySecondObj, typeField, enumConstant);
+	
 	// Add the objects to the ArrayList
 	(*env)->CallBooleanMethod(env, myList, arrayListAdd, myObj);
 	(*env)->CallBooleanMethod(env, myList, arrayListAdd, mySecondObj);
